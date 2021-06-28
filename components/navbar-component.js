769d9a8de38af67a6d5fd56d4a357d190573e22f
Yaniel Llerena <jinyo24@gmail.com>
2021-06-28 09:31:10 -0500

    General Improvement, Replaced Contact Me page with Dropdown Menu

--- components/navbar-component.js
+++ components/navbar-component.js
@@ -1,11 +1,11 @@
 class Nav extends HTMLElement {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    connectedCallback() {
-        this.innerHTML =
-`<nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top"
+  connectedCallback() {
+    this.innerHTML =
+      `<nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top"
       style="max-width: 100%; box-shadow: 0px 7px 7px 0px rgba(0,0,0,0.5);">
       <a class="navbar-brand" style="margin-left: 20px;" href="https://github.com/jinyo24">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" class="bi bi-github"
@@ -18,16 +18,24 @@ class Nav extends HTMLElement {
         style="margin-right: 10px;">
         <span class="navbar-toggler-icon"></span>
       </button>
+
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav m-lg-auto m-md-auto m-sm-auto ms-4">
-          <li class="nav-item"> <a class="nav-link" href="index.html">Home</a> </li>
-          <li class="nav-item"> <a class="nav-link" href="portfolio.html">Projects</a> </li>
-          <li class="nav-item"> <a class="nav-link" href="">Contact Me</a> </li>
+        <ul class="navbar-nav ms-auto me-2">
+          <li class="nav-item ms-3"> <a class="nav-link active" href="index.html">Home</a> </li>
+          <li class="nav-item ms-3"> <a class="nav-link" href="portfolio.html">Projects</a> </li>
+          <li class="nav-item dropdown ms-3"> 
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Contact Me</a> 
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown" style="left: auto; right: 0;">
+                <li><a class="dropdown-item" href="https://www.linkedin.com/in/yllerena/" target="_blank"> <i class="bi bi-linkedin"></i> yllerena</a></li>
+                <li><a class="dropdown-item" href="https://twitter.com/yllerena1987" target="_blank"><i class="bi bi-twitter"></i> @yllerena1987</a></li>
+                <li><a class="dropdown-item" href="mailto:yllerena1987@gmail.com"> <i class="bi bi-envelope-fill"></i> yllerena1987@gmail.com</a></li>
+              </ul>   
+          </li>
         </ul>
       </div>
-    </nav>`;
+    </nav>`
 
-    }
+  }
 }
 
 customElements.define('navbar-component', Nav);
